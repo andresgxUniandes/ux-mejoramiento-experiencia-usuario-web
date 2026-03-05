f56f3ed6fce8870a73d63eb2296e08876efbd873
andres-uniandes <andres-uniandes@andres-macs-MacBook-Pro-2.local>
2026-03-04 21:13:54 -0500

    Se agrega screen welcome con botones ingresar y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/welcome/welcome.component').then(m => m.WelcomeComponent) },
+  { path: 'ingresar', loadComponent: () => import('./pages/welcome/welcome.component').then(m => m.WelcomeComponent) }, // TODO: pantalla de login
+  { path: 'registrar', loadComponent: () => import('./pages/welcome/welcome.component').then(m => m.WelcomeComponent) }, // TODO: pantalla de registro
+];
